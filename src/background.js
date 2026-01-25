c42af0311d1891e5e750c65651e21d0821a4e0f0
Nick Dower <nicholasdower@gmail.com>
2026-01-25 19:07:33 +0100

    Show badge icon on refresh error.

--- src/background.js
+++ src/background.js
@@ -19,3 +19,26 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
     chrome.runtime.openOptionsPage();
   }
 });
+
+async function updateErrorBadge() {
+  const sources = await config.loadSources();
+  const remote = config.getRemoteSources(sources);
+  const hasErrors = remote.some(s => s.error);
+  
+  if (hasErrors) {
+    chrome.action.setBadgeText({ text: '!' });
+    chrome.action.setBadgeBackgroundColor({ color: '#dc2626' });
+  } else {
+    chrome.action.setBadgeText({ text: '' });
+  }
+}
+
+// Update badge when sources change
+chrome.storage.onChanged.addListener((changes, areaName) => {
+  if (areaName === 'local' && changes[config.STORAGE_KEY]) {
+    updateErrorBadge();
+  }
+});
+
+// Check on startup
+updateErrorBadge();
