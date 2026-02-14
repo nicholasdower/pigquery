d2a282f28dae380c27e9906f1ae114b21698ea7f
Nick Dower <nicholasdower@gmail.com>
2026-02-14 10:11:54 +0100

    Lazy load version for logger in case extension not loaded.

--- src/logger.js
+++ src/logger.js
@@ -1,10 +1,25 @@
-const NAME = `PigQuery v${chrome.runtime.getManifest().version} ${process.env.NODE_ENV}`;
+// Lazy load the name to avoid errors when the extension is not yet loaded
+const getName = (() => {
+  let cached;
+  return () => {
+    if (cached) return cached;
+    try {
+      const version = chrome?.runtime?.getManifest()?.version;
+      if (version) {
+        cached = `PigQuery v${version} ${process.env.NODE_ENV}`;
+        return cached;
+      }
+    } catch (e) {
+      return `PigQuery v(unknown) ${process.env.NODE_ENV}`;
+    }
+  };
+})();
 
 const logger = {
-  log: (...args) => console.log(`${new Date().toISOString()} - LOG - ${NAME} - `, ...args),
-  warn: (...args) => console.warn(`${new Date().toISOString()} - WARN - ${NAME} - `, ...args),
-  error: (...args) => console.error(`${new Date().toISOString()} - ERROR - ${NAME} - `, ...args),
-  debug: (...args) => console.debug(`${new Date().toISOString()} - DEBUG - ${NAME} - `, ...args),
+  log: (...args) => console.log(`${new Date().toISOString()} - LOG - ${getName()} - `, ...args),
+  warn: (...args) => console.warn(`${new Date().toISOString()} - WARN - ${getName()} - `, ...args),
+  error: (...args) => console.error(`${new Date().toISOString()} - ERROR - ${getName()} - `, ...args),
+  debug: (...args) => console.debug(`${new Date().toISOString()} - DEBUG - ${getName()} - `, ...args),
 };
 
 export default logger;
